87b71cba0596461f444ac582feb1875a3be5b1a8
Christopher Cohen <ender9889@gmail.com>
2022-10-30 11:39:04 -0700

    Refactor all N+1 queries and reformat code

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -14,19 +14,13 @@ router.delete(
   restoreUser,
   async (req, res, next) => {
     const { user } = req;
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication required', statusCode: 401 });
-    }
+    if (!user) return res.status(401).json({ message: 'Authentication required', statusCode: 401 });
 
     const img = await ReviewImage.findByPk(req.params.imageId);
-    if (!img) {
-      return res.status(404).json({ message: "Spot Image couldn't be found", statusCode: 404 })
-    }
+    if (!img) return res.status(404).json({ message: "Spot Image couldn't be found", statusCode: 404 })
 
     const review = await Review.findByPk(img.reviewId)
-    if (review.userId != user.id) {
-      return res.status(403).json({ message: "Forbidden", statusCode: 403 });
-    }
+    if (review.userId != user.id) return res.status(403).json({ message: "Forbidden", statusCode: 403 });
 
     await img.destroy()
     res.json({
--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -14,21 +14,16 @@ router.delete(
   restoreUser,
   async (req, res, next) => {
     const { user } = req;
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication required', statusCode: 401 });
-    }
+    if (!user) return res.status(401).json({ message: 'Authentication required', statusCode: 401 });
 
     const img = await SpotImage.findByPk(req.params.imageId);
-    if (!img) {
-      return res.status(404).json({ message: "Spot Image couldn't be found", statusCode: 404 })
-    }
+    if (!img) return res.status(404).json({ message: "Spot Image couldn't be found", statusCode: 404 });
 
     const spot = await Spot.findByPk(img.spotId)
-    if (spot.ownerId != user.id) {
-      return res.status(403).json({ message: "Forbidden", statusCode: 403 });
-    }
+    if (spot.ownerId != user.id) return res.status(403).json({ message: "Forbidden", statusCode: 403 });
 
     await img.destroy()
+
     res.json({
       message: "Successfully deleted",
       statusCode: 200
