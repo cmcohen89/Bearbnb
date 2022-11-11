8af16b5b42a013e588cd95ea91caded62ee8e853
Christopher Cohen <ender9889@gmail.com>
2022-11-11 12:28:18 -0800

    Add basic styling

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <button class='profile' onClick={openMenu}>
+        <i className="fas fa-user-circle" />
+      </button>
+      {showMenu && (
+        <ul class='profile-dropdown' className="profile-dropdown">
+          <li class='profile-dropdown'>{user.username}</li>
+          <li class='profile-dropdown'>{user.email}</li>
+          <li class='profile-dropdown'>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default ProfileButton;
