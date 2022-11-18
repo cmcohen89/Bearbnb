b3f8ad4a13c6d524cfc54bc8e82822e3cefa825f
Christopher Cohen <ender9889@gmail.com>
2022-11-18 10:15:05 -0800

    footer links

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer>
       <div className="footer">
-        <div className='footer-left'>© 2022 Bearbnb, Inc. · Privacy · Terms · Sitemap · Destinations</div>
+        <div className='footer-left'>© 2022 Bearbnb, Inc. · <NavLink className='plain-link-gray' to='/coming-soon'>Privacy</NavLink> · <NavLink className='plain-link-gray' to='/coming-soon'>Terms</NavLink> · <NavLink className='plain-link-gray' to='/coming-soon'>Sitemap</NavLink> · <NavLink className='plain-link-gray' to='/coming-soon'>Destinations</NavLink></div>
         <div className='footer-right'>
           <NavLink to='/coming-soon'><i class="fa-solid fa-globe globe2"></i></NavLink>
           <NavLink className='plain-link-gray' to='/coming-soon'><span>English (US)</span></NavLink>
