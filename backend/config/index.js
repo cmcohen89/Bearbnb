6f74f11f0fe74f12c5a7a644abf2da946f28ee30
Christopher Cohen <ender9889@gmail.com>
2022-11-20 20:49:35 -0800

    node env set to production

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  environment: process.env.NODE_ENV || 'development',
+  environment: process.env.NODE_ENV || 'production',
   port: process.env.PORT || 8000,
   dbFile: process.env.DB_FILE,
   jwtConfig: {
