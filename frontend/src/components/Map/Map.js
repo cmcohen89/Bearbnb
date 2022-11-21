b9ab2a2759fc765810d3aa53a8cc9f6cfa587f3e
Christopher Cohen <ender9889@gmail.com>
2022-11-20 22:03:38 -0800

    one more try for dynamic map

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -3,24 +3,23 @@ import './Map.css';
 
 const Map = ({ spot }) => {
 
-  const environment = 'AIzaSyBSTJdNRbbb3l-tDELReRQ0O7ibW6BUGOU';
-
   return (
-    <>
-      <h3 className='map-header'>Where you'll be</h3>
-      <iframe
-        id='map'
-        className='embed-map'
-        title='location-map'
-        src={`https://www.google.com/maps/embed/v1/place?key=${environment}&q=${spot.city}+${spot.state}`}>
-      </iframe>
-      <p className='stat-header-item'>{spot.city}, {spot.state}, {spot.country}</p>
-      <p className='spot-description'>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-        quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-      </p>
-    </>
+    <MapContainer spot={spot} />
+    // <>
+    //   <h3 className='map-header'>Where you'll be</h3>
+    //   <iframe
+    //     id='map'
+    //     className='embed-map'
+    //     title='location-map'
+    //     src={`https://www.google.com/maps/embed/v1/place?key=${environment}&q=${spot.city}+${spot.state}`}>
+    //   </iframe>
+    //   <p className='stat-header-item'>{spot.city}, {spot.state}, {spot.country}</p>
+    //   <p className='spot-description'>
+    //     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+    //     totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+    //     quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+    //   </p>
+    // </>
   )
 }
 
